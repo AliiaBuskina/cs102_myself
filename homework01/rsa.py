f040b2d190d34692b7ffd50fea3a20261edcb6b4
AliiaBuskina <buskina2003aliya@gmail.com>
2023-04-06 13:20:52 +0300

    Реализованы функции multiplicative_inverse() и generate_keypair()

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -1,7 +1,6 @@
 import random
 import typing as tp
 
-
 def is_prime(n: int) -> bool:
     """
     Tests to see if a number is prime.
@@ -13,9 +12,18 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
-    pass
-
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 
 def gcd(a: int, b: int) -> int:
     """
@@ -26,8 +34,12 @@ def gcd(a: int, b: int) -> int:
     >>> gcd(3, 7)
     1
     """
-    # PUT YOUR CODE HERE
-    pass
+    while a != 0 and b != 0:
+        if a > b:
+            a = a % b
+        else:
+            b = b % a
+    return a + b
 
 
 def multiplicative_inverse(e: int, phi: int) -> int:
@@ -38,8 +50,22 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     >>> multiplicative_inverse(7, 40)
     23
     """
-    # PUT YOUR CODE HERE
-    pass
+    s, old_s = 0, 1
+    t, old_t = 1, 0
+    r, old_r = phi, e
+
+    while r != 0:
+        quotient = old_r // r
+        old_r, r = r, old_r - quotient * r
+        old_s, s = s, old_s - quotient * s
+        old_t, t = t, old_t - quotient * t
+
+    gcd, x, y = old_r, old_s, old_t
+
+    if gcd != 1:
+        raise ValueError("No multiplicative inverse exists")
+
+    return x % phi
 
 
 def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
@@ -48,26 +74,14 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
     elif p == q:
         raise ValueError("p and q cannot be equal")
 
-    # n = pq
-    # PUT YOUR CODE HERE
-
-    # phi = (p-1)(q-1)
-    # PUT YOUR CODE HERE
-
-    # Choose an integer e such that e and phi(n) are coprime
+    n=p*q
+    phi=(p-1)*(q-1)
     e = random.randrange(1, phi)
-
-    # Use Euclid's Algorithm to verify that e and phi(n) are coprime
     g = gcd(e, phi)
     while g != 1:
         e = random.randrange(1, phi)
         g = gcd(e, phi)
-
-    # Use Extended Euclid's Algorithm to generate the private key
     d = multiplicative_inverse(e, phi)
-
-    # Return public and private keypair
-    # Public key is (e, n) and private key is (d, n)
     return ((e, n), (d, n))
 
 
--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -10,8 +10,25 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    ciphertext = ""
+    if len(plaintext) > len(keyword):
+        keyword *= len(plaintext) // len(keyword) + 1
+    for i in range(len(plaintext)):
+        bc = plaintext[i]
+        k = keyword[i]
+        if k.isupper():
+            sd = ord(k) - ord("A")
+        if k.islower():
+            sd = ord(k) - ord("a")
+        if bc.isalpha():
+            if bc.isupper():
+                bc = chr((ord(bc) + sd - ord("A")) % 26 + ord("A"))
+            if bc.islower():
+                bc = chr((ord(bc) + sd - ord("a")) % 26 + ord("a"))
+        ciphertext += bc
+
     return ciphertext
+
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
@@ -26,5 +43,23 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    if len(ciphertext) > len(keyword):
+        keyword *= len(ciphertext) // len(keyword) + 1
+    for i in range(len(ciphertext)):
+        bd = ciphertext[i]
+        k = keyword[i]
+        if k.isupper():
+            sd = ord(k) - ord("A")
+        elif k.islower():
+            sd = ord(k) - ord("a")
+        else:
+            sd = 0
+        if bd.isalpha():
+            if bd.isupper():
+                bc = chr((ord(bd) - sd - ord("A")) % 26 + ord("A"))
+            else:
+                bc = chr((ord(bd) - sd - ord("a")) % 26 + ord("a"))
+        else:
+            bc = bd
+        plaintext += bc
     return plaintext
